7b6582e537886bc93b0b39321f8a6189e8003795
Sweta <swetadhandapani2@gmail.com>
2025-10-02 20:55:41 +0530

    Modified CI flow

--- backend/tests/setupEnv.js
+++
@@ -1,5 +0,0 @@
-process.env.SENDGRID_API_KEY = "SG.fakeKey123";
-process.env.GROQ_API_KEY = "gsk.fakeKey123";
-process.env.OPENAI_API_KEY = "sk.fakeKey123";
-process.env.JWT_SECRET = "testsecret";
-process.env.MONGO_URI = "mongodb://localhost:27017/testdb";

399e48413055611b9145ec259b31fe4472b1c0b0
Sweta <swetadhandapani2@gmail.com>
2025-10-02 19:55:46 +0530

    Modified test files.

---
+++ backend/tests/setupEnv.js
@@ -0,0 +1,5 @@
+process.env.SENDGRID_API_KEY = "SG.fakeKey123";
+process.env.GROQ_API_KEY = "gsk.fakeKey123";
+process.env.OPENAI_API_KEY = "sk.fakeKey123";
+process.env.JWT_SECRET = "testsecret";
+process.env.MONGO_URI = "mongodb://localhost:27017/testdb";

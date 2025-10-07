c8501be717fd50c9fb8da6e6bc25de8ff25a9185
Sweta <swetadhandapani2@gmail.com>
2025-10-07 19:40:29 +0530

    Modified api.

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL || 'https://ai-study-assistant-pumn.onrender.com/api';
+const API_BASE =
+  import.meta.env.VITE_API_URL ||
+  (window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"
+    : `http://${window.location.hostname}:5000/api`);
+
 
 const instance = axios.create({
   baseURL: API_BASE
